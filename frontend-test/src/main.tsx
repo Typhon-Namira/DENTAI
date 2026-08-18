a02d549aca12c1d5460e98b6752641e7e597b75b
typhon Namira <tufannamira@gmail.com>
2026-08-18 21:10:35 +0400

    load medical workspace styles

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/workspace-redesign.css";
+import "./styles/medical-workspace.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
