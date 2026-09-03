090f0f8ea6ee8a372cfca8368aaeca145d21d9d6
typhon Namira <tufannamira@gmail.com>
2026-09-03 13:47:33 +0400

    Load real OPG and Armenian UI refinements

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppV4 from "./v4/AppV4";
 import "./v4/v4.css";
+import "./v4/v4-real-opg.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
