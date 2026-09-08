9e7535833c86854264945a6e1d012b005bbe5270
typhon Namira <tufannamira@gmail.com>
2026-09-08 15:21:26 +0400

    Mount integrated Teta2 Care command center

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ProductApp from "./product/ProductApp";
+import { CareCommandCenter } from "./product/CareCommandCenter";
 import "./styles/medical-workspace.css";
 import "./v4/v4.css";
 import "./v4/v4-real-opg.css";
@@ -8,9 +9,11 @@ import "./v4/v4-clinical-tools.css";
 import "./v4/v4-opg-findings.css";
 import "./v4/v4-opg-coordinate-fix.css";
 import "./product/product.css";
+import "./product/care-command.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ProductApp />
+    <CareCommandCenter />
   </StrictMode>
 );
