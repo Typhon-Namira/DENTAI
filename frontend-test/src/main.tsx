c227142406fa58b1e702bd50ad302291ccc284f1
typhon Namira <tufannamira@gmail.com>
2026-09-09 12:27:05 +0400

    Remove duplicate Care command center mount

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import ProductApp from "./product/ProductApp";
-import { CareCommandCenter } from "./product/CareCommandCenter";
 import "./styles/medical-workspace.css";
 import "./v4/v4.css";
 import "./v4/v4-real-opg.css";
@@ -9,11 +8,9 @@ import "./v4/v4-clinical-tools.css";
 import "./v4/v4-opg-findings.css";
 import "./v4/v4-opg-coordinate-fix.css";
 import "./product/product.css";
-import "./product/care-command.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ProductApp />
-    <CareCommandCenter />
   </StrictMode>
 );
