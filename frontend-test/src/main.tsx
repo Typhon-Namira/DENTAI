8ea554dee03c5efdc89f7540d480b852c337a4e8
typhon Namira <tufannamira@gmail.com>
2026-09-11 13:56:14 +0400

    Mount platform-aware root router

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { RootApp } from "./RootApp";
 import { CareExperienceEnhancer } from "./product/CareExperienceEnhancer";
 import { CareGenerationContractPanel } from "./product/CareGenerationContractPanel";
 import { FollowupCaseWorkspace } from "./product/FollowupCaseWorkspace";
-import ProductApp from "./product/ProductApp";
 import "./styles/medical-workspace.css";
+import "./styles/teta2-platform.css";
 import "./v4/v4.css";
 import "./v4/v4-real-opg.css";
 import "./v4/v4-clinical-tools.css";
@@ -14,14 +15,15 @@ import "./v4/v4-opg-coordinate-fix.css";
 import "./product/product.css";
 import "./product/care-experience.css";
 import "./product/followup-case.css";
+import "./product/platform-access.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <>
-      <ProductApp />
+      <RootApp />
       <CareExperienceEnhancer />
       <CareGenerationContractPanel />
       <FollowupCaseWorkspace />
     </>
-  </StrictMode>
+  </StrictMode>,
 );
