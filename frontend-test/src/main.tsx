6eef306001b4b8449223dfb8bb2361792d2b0b9e
typhon Namira <tufannamira@gmail.com>
2026-09-12 23:09:22 +0400

    Load final homepage hero fix

--- frontend-test/src/main.tsx
+++ frontend-test/src/main.tsx
@@ -23,6 +23,7 @@ import "./product/home-hero-fit.css";
 import "./product/home-hero-fit-v2.css";
 import "./product/home-hero-fit-v3.css";
 import "./product/home-hero-workflow.css";
+import "./product/home-hero-workflow-fix.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
